c67074093c3f50035a698595727c3b48e632e8d3
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-30 23:46:40 +0200

    Wire useToast hook to ToastContext

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,14 +1,3 @@
 "use client";
 
-// TODO (Step 6): Implement with a toast queue and portal.
-// Will expose: toast.success(), toast.error(), toast.info().
-export function useToast() {
-  return {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    success: (_message: string) => {},
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    error: (_message: string) => {},
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    info: (_message: string) => {},
-  };
-}
+export { useToastContext as useToast } from "@/contexts/toast-context";
