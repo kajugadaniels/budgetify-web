fa6fcc9275848d3a27621228af5a9760f9d6dfa6
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-31 19:08:29 +0200

    Add src/constant/todos/upload.ts todo upload limits

---
+++ src/constant/todos/upload.ts
@@ -0,0 +1,7 @@
+export const MAX_TODO_IMAGES = 6;
+export const MAX_TODO_IMAGE_SIZE_BYTES = 10 * 1024 * 1024;
+export const ALLOWED_TODO_IMAGE_MIME_TYPES = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+] as const;
